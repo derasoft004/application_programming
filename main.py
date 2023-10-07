51752a075be6f9b8c7c33de3a917c41617c4f1f8
egor <derasoft004@gmail.com>
2023-10-07 15:08:18 +0400

    готовый алгоритм скачивания более тысячи изображений по запросу "тигр" и "леопард" и сохранения их в соответствующую папку

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import os
 import time
-import cv2
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import urllib.request
@@ -10,56 +9,70 @@ def driver_scroller(driver_to_scroll, pix):
     sroll_range = 0
     while sroll_range < pix:
         driver_to_scroll.execute_script(f"window.scrollTo(0, {sroll_range});")  # ширина, высота
-        sroll_range += 3  # скорость
+        sroll_range += 4  # скорость
+    while sroll_range > 0:
+        driver_to_scroll.execute_script(f"window.scrollTo(0, {sroll_range});")  # ширина, высота
+        sroll_range -= 4
 
 
-full_list = []
+full_list_tiger, full_list_leopard = [], []
 count_pictures = 0
 
-URLs = ["https://ya.ru/images/search?from=tabbar&text=tiger",
-        "https://ya.ru/images/search?from=tabbar&lr=11135&p=10&rpt=image&text=tiger",
-        "https://ya.ru/images/search?from=tabbar&lr=11135&p=20&rpt=image&text=tiger",
-        "https://ya.ru/images/search?from=tabbar&lr=11135&p=30&rpt=image&text=tiger"]
+URLs_tiger = ["https://ya.ru/images/search?from=tabbar&text=tiger",
+            "https://ya.ru/images/search?from=tabbar&lr=11135&p=10&rpt=image&text=tiger",
+            "https://ya.ru/images/search?from=tabbar&lr=11135&p=21&rpt=image&text=tiger",
+            "https://ya.ru/images/search?from=tabbar&lr=11135&p=30&rpt=image&text=tiger",
+            "https://ya.ru/images/search?from=tabbar&lr=11135&p=40&rpt=image&text=tiger"]
+
+URLs_leopard = ["https://ya.ru/images/search?from=tabbar&text=leopard",
+                "https://ya.ru/images/search?from=tabbar&lr=11135&p=10&rpt=image&text=leopard",
+                "https://ya.ru/images/search?from=tabbar&lr=11135&p=20&rpt=image&text=leopard",
+                "https://ya.ru/images/search?from=tabbar&lr=11135&p=30&rpt=image&text=leopard",
+                "https://ya.ru/images/search?from=tabbar&lr=11135&p=40&rpt=image&text=leopard"]
 
 pictures_count, page_count = 0, 1
 
-# def find_button_next_page(driver): не находит хотя кнопка есть
-#     button = driver.find_element(By.XPATH, "//a[@class='button2 button2_size_l button2_theme_action button2_type_link button2_view_classic more__button i-bem button2_js_inited']")
-#     print(button)
 
-
-def make_driver_with_link(link, path_name):
-    global full_list, count_pictures
+def make_driver_with_link(link, path_name, animal):
+    global full_list_tiger, full_list_leopard
     driver = webdriver.Chrome()
     driver.get(link)
     print(driver.title)
     time.sleep(4)
     driver_scroller(driver, 15000)
     list_pictures = driver.find_elements(By.XPATH, path_name)
-    count_pictures += len(list_pictures)
-    print(count_pictures)
-    full_list += list_pictures
+    if animal == "tiger": full_list_tiger += list_pictures
+    elif animal == "leopard": full_list_leopard += list_pictures
+    print(len(full_list_tiger), len(full_list_leopard))
 
 
-for i in range(4):
-    make_driver_with_link(URLs[i], "//img[@class='serp-item__thumb justifier__thumb']")
-    time.sleep(20)
+for i in range(5):
+    if len(full_list_tiger) <= 1000:
+        make_driver_with_link(URLs_tiger[i], "//img[@class='serp-item__thumb justifier__thumb']", "tiger")
+        time.sleep(10)
+    if len(full_list_leopard) <= 1000:
+        make_driver_with_link(URLs_leopard[i], "//img[@class='serp-item__thumb justifier__thumb']", "leopard")
+        time.sleep(10)
 
 
-if not os.path.exists('dataset'):
-    os.mkdir('dataset')
-if not os.path.exists('dataset/tiger'):
-    os.mkdir('dataset/tiger')
+if not os.path.exists('dataset'): os.mkdir('dataset')
+if not os.path.exists('dataset/tiger'): os.mkdir('dataset/tiger')
+if not os.path.exists('dataset/leopard'): os.mkdir('dataset/leopard')
 
 
 def save_pictures():
-    global full_list
-    directory = "dataset/tiger"
-    for i in range(len(full_list)):
-        img = urllib.request.urlopen(full_list[i].get_attribute('src')).read()
-        print(full_list[i].get_attribute('src'))
-        out = open(f"{directory}/image{i+1}.jpg", "wb")
+    global full_list_tiger, full_list_leopard
+    directory_tiger, directory_leopard = "dataset/tiger", "dataset/leopard"
+    print(f'lens: {len(full_list_tiger)}, {len(full_list_leopard)}')
+    for elem in range(len(full_list_tiger)):
+        img = urllib.request.urlopen(full_list_tiger[elem].get_attribute('src')).read()
+        out = open(f"{directory_tiger}/image{elem}.jpg", "wb")
         out.write(img)
+        out.close
+    for elem in range(len(full_list_leopard)):
+        img2 = urllib.request.urlopen(full_list_leopard[elem].get_attribute('src')).read()
+        out = open(f"{directory_leopard}/image{elem}.jpg", "wb")
+        out.write(img2)
         out.close
 
 save_pictures()
