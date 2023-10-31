45ee2da3ed02d0f37e17d46de893f268d84c6e78
Egor <derasoft004@gmail.com>
2023-10-31 11:39:40 +0400

    class iterator laba 2

--- main.py
+++ main.py
@@ -76,21 +76,10 @@ def save_pictures():
         out.close
 
 
-def main_first():
-    for i in range(5):
-        if len(full_list_tiger) <= 1000:
-            make_driver_with_link(URLs_tiger[i], "//img[@class='serp-item__thumb justifier__thumb']", "tiger")
-            time.sleep(10)
-        if len(full_list_leopard) <= 1000:
-            make_driver_with_link(URLs_leopard[i], "//img[@class='serp-item__thumb justifier__thumb']", "leopard")
-            time.sleep(10)
-    save_pictures()
-    annotation_maker('dataset/annotation.csv')
 
-
-def list_dataset(animal):
-    return [f'{os.getcwd()}/dataset/{animal}/{x}' for x in os.listdir(f'dataset/{animal}')], [f'dataset/{animal}/{x}' for x in os.listdir(f'dataset/{animal}')]
-
+def list_dataset(dataset, animal):
+    return [f'{os.getcwd()}/{dataset}/{animal}{x}' for x in os.listdir(f'{dataset}/{animal}')], [f'{dataset}/{animal}{x}' for x in os.listdir(f'{dataset}/{animal}')]
+    # когда передаем animal - добавлять /
 
 def loop_for_writing(csvfile, full_list_t: list, list_t: list,
                      full_list_l: list, list_l: list, t, l):
@@ -99,12 +88,12 @@ def loop_for_writing(csvfile, full_list_t: list, list_t: list,
         if i < len(list_l): csvfile.writerow({'Full path': full_list_l[i], 'Relative path': list_l[i], 'class': l})
 
 
-def annotation_maker(path: str):
+def annotation_maker(dataset, path: str, t, l):
     global tig, leo
     list_relative_path_images_tiger, list_relative_path_images_leopard = \
-        list_dataset(tig)[1], list_dataset(leo)[1]
+        list_dataset(dataset, t)[1], list_dataset(dataset, l)[1]
     list_full_path_images_tiger, list_full_path_images_leopard = \
-        list_dataset(tig)[0], list_dataset(leo)[0]
+        list_dataset(dataset, t)[0], list_dataset(dataset, l)[0]
     with open(path, 'w', newline='') as csvfile:
         fieldnames = ['Full path', 'Relative path', 'class']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
@@ -112,6 +101,19 @@ def annotation_maker(path: str):
         loop_for_writing(writer, list_full_path_images_tiger, list_relative_path_images_tiger,
                          list_full_path_images_leopard, list_relative_path_images_leopard,
                          tig, leo)
+
+def main_first():
+    def parser():
+        for i in range(5):
+            if len(full_list_tiger) <= 1000:
+                make_driver_with_link(URLs_tiger[i], "//img[@class='serp-item__thumb justifier__thumb']", "tiger")
+                time.sleep(10)
+            if len(full_list_leopard) <= 1000:
+                make_driver_with_link(URLs_leopard[i], "//img[@class='serp-item__thumb justifier__thumb']", "leopard")
+                time.sleep(10)
+    # parser()
+    # save_pictures()
+    annotation_maker('dataset', 'dataset/annotation.csv', tig+'/', leo+'/')
 
 
 def copy_dataset(animal): # по animal копируется все содержимое в new_dataset
@@ -127,8 +129,28 @@ def copy_dataset(animal): # по animal копируется все содерж
 def main_second():
     copy_dataset(tig)
     copy_dataset(leo)
-    annotation_maker('new_dataset/1new_dataset_annotation.csv')
+    annotation_maker('new_dataset', 'new_dataset/1new_dataset_annotation.csv', '', '')
 
+
+class Iterator:
+    def __init__(self, animal_list, lim):
+        self.animal_list = animal_list
+        self.lim = lim
+        self.count = 0
+
+    def __next__(self):
+        if self.count < self.lim:
+            self.count += 1
+            return self.animal_list[self.count - 1]
+        else:
+            raise StopIteration
+
+# photos = Iterator(lst, 7)
+#
+# try:
+#     print(next(photos))
+# except StopIteration:
+#     print("it's all")
 
 if __name__ == "__main__":
     # main_first()
