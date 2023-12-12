ac97b048028fa8f3cf5df0559be02932537360ae
Egor <derasoft004@gmail.com>
2023-12-12 08:01:56 +0400

    lab4 10 tasks are finaly

--- laba4.py
+++ laba4.py
@@ -1,8 +1,13 @@
 import pandas as pd
+import numpy as np
 from main import list_dataset
 import os
 from PIL import Image
-from typing import Tuple
+from typing import Tuple, List
+import cv2
+import matplotlib.pyplot as plt
+
+
 
 
 """
@@ -11,6 +16,10 @@ data - [[строчка]
         [строчка]]
 index - [индексы вертикально]
 columns - [индексы горизонтально]
+
+df.loc['mark'] доступ по метке к группе элементов
+df[df['mark']==...] для сортировки по конкретной колонке
+df['height'].value_counts() счетчик одинаковых значений
 """
 
 tiger_list, leopard_list = \
@@ -75,36 +84,83 @@ def task4(df: pd.DataFrame) -> pd.DataFrame:
 
     return df
 
-print(task4(task3(task12()[0], task12()[1])))
-
-# df.loc['r1'] доступ по метке к группе элементов
+# print(task4(task3(task12()[0], task12()[1])))
+full_params_df = task4(task3(task12()[0], task12()[1]))
 
 def task5(df: pd.DataFrame) -> pd.DataFrame:
     """
-    values = (height, width, depth, mark)
+    считает средние статистические данные
+    :param df:
+    :return: dataframe со статистикой по всем данным
+    """
+    statistic_tiger = df[df['mark'] == 0].describe()
+    statistic_leopard = df[df['mark'] == 1].describe()
+    eps = 10
+    balanced_flag = '\nset is balanced\n'
+    if abs(statistic_tiger['width'].mean() - statistic_leopard['width'].mean()) >= eps:
+        balanced_flag = '\nset is not balanced\n'
+    return df[['width', 'height']].describe()
+
+# print(task5(task4(task3(task12()[0], task12()[1]))))
+
+def task_6_new_df(df: pd.DataFrame, mark: int) -> pd.DataFrame:
+    return df[df['mark'] == mark]
+
+# print(task_6_new_df(full_params_df, 0))
+
+def task_7(df: pd.DataFrame, mark: int, height_max: int, width_lim: int) -> pd.DataFrame:
+    return df[(df['mark'] == mark) & (df['height'] <= height_max) & (df['width'] <= width_lim)]
+
+# print(task_7(full_params_df, 0, full_params_df['height'].max(), full_params_df['width'].max()))
+
+def task_8(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    добавляем колонку с подсчетом пикселей
     :param df:
     :return:
     """
-    height, width, depth, mark = df['height'], df['width'], df['depth'], df['mark']
-    values: Tuple = height.value_counts(), width.value_counts(), depth.value_counts(), mark.value_counts()
-    print(len(height), len(width), len(depth), len(mark))
-    print(len(values[0]), len(values[1]), len(values[2]), len(values[3]))
-    mark_0, mark_1 = [], []
-    for ind in range(len(height)):
-        if mark[ind]: mark_1.append([height[ind], width[ind], depth[ind]])
-        else: mark_0.append([height[ind], width[ind], depth[ind]])
-    # print(mark_1, mark_0, sep='\n\n')
-
+    df['pixels'] = df['width'] * df['height'] * df['depth']
     return df
 
-task5(task4(task3(task12()[0], task12()[1])))
+full_params_df = task_8(full_params_df)
 
+def task_9(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    добавляем колонку с подсчетом пикселей
+    :param df:
+    :return:
+    """
+    grouped = df.groupby('animal').agg({'pixels': ['max', 'min', 'mean']})
+    return grouped
 
+# print(task_9(full_params_df))
 
+def get_hists(df: pd.DataFrame, class_name: int) -> List[np.ndarray]:
+    """
+    Возвращает 3 массива значений гистограммы по каждому каналу(b, g, r)
+    """
+    class_df = task_6_new_df(df, class_name)
+    img = cv2.imread(class_df['absolute_path'].sample().values[0])
+    hists = []
+    for i in range(3):
+        hist = cv2.calcHist([img], [i], None, [256], [0, 256])
+        hists.append(hist)
+    return hists
 
+print(get_hists(full_params_df, 0))
 
+hists = get_hists(full_params_df, 1)
+colors = ["Blue", "Green", "Red"]
 
+for i in range(len(hists)):
+    plt.plot(hists[i], color=colors[i], label=f"{colors[i]} Histogram")
+    plt.xlim([0, 256])
 
+plt.title("Histograms")
+plt.xlabel('Intensity', fontsize=13)
+plt.ylabel('Density Pixels', fontsize=13)
+plt.legend()
+plt.show()
 
 
 
